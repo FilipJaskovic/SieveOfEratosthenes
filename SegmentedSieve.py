c47c94be7b81bc6f437c6ddecdb84ff029fbb59a
Filip Jaskovic <75838849+BuBo94@users.noreply.github.com>
2023-10-07 22:32:15 +0200

    Optimize Sieve and Segmented Sieve Algorithms
    Changes:
    - Refactor `simple_sieve` to utilize slice assignment for enhanced performance.
    - Simplify the marking of non-prime numbers by eliminating nested loops in favor of slice operations.
    - Improve clarity in `segmented_sieve` with list comprehensions for collecting primes in segments.
    - Optimize the start point calculation in the segment for each prime to boost efficiency.
    - Use the `extend` method combined with list comprehension for a clearer and more efficient way of adding primes from each segment to the result.
    
    Benefits:
    - The refactored code is more performant due to optimized Python constructs.
    - Enhanced readability and maintainability.

--- SegmentedSieve.py
+++ SegmentedSieve.py
@@ -1,32 +1,34 @@
 def simple_sieve(n):
-    sieve = [True] * (n+1)
-    sieve[0], sieve[1] = False, False
+    sieve = [True] * (n + 1)
+    sieve[:2] = [False, False]  
+    
     for i in range(2, int(n**0.5) + 1):
         if sieve[i]:
-            for j in range(i*i, n+1, i):
-                sieve[j] = False
-    return [i for i, v in enumerate(sieve) if v]
+            sieve[i*i:n+1:i] = [False] * len(range(i*i, n+1, i))
+    
+    return [i for i, prime in enumerate(sieve) if prime]
 
 def segmented_sieve(n):
     delta = int(n**0.5) + 1
     primes = simple_sieve(delta)
-    result = list(primes)
+    result = list(primes)  
     
     for m in range(delta, n + 1, delta):
         segment = [True] * delta
         lower_limit = m - delta + 1
         
         for prime in primes:
-            start = max(prime * prime, (lower_limit // prime + (lower_limit % prime > 0)) * prime)
-            for j in range(start, m + 1, prime):
-                segment[j - lower_limit] = False
+            start = max(prime * prime, (lower_limit + prime - 1) // prime * prime)
+            segment_start = start - lower_limit
+            segment[segment_start:m-lower_limit+1:prime] = [False] * len(range(segment_start, m-lower_limit+1, prime))
         
-        for i, is_prime in enumerate(segment):
-            if is_prime and i + lower_limit <= n:
-                result.append(i + lower_limit)
+        result.extend([i + lower_limit for i, is_prime in enumerate(segment) if is_prime])
                 
     return result
 
-n = int(input("Enter the upper limit n: "))
-print(segmented_sieve(n))
+if __name__ == "__main__":
+    n = int(input("Enter the upper limit n: "))
+    print(segmented_sieve(n))
+
+
 
